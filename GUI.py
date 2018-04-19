956b4e204a0ad0f9e23a7517f3777000868d2382
Matt Hires <mettihires@gmail.com>
2018-04-19 21:37:21 +0200

    updated settings menu-must find out how to handle the checkbox, added signup frame-without functionality

--- GUI.py
+++ GUI.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python
 from tkinter import *
+from tkinter.font import Font
 
-# WARNING!
-#
-# treba este dodat zopar veci, zatial tolko funkcionalit som zvladol pridat..
-# treba aspon tie produkty poriesit..nemusi byt uplne funkcny program..
-# + ten padding nejak zrusit, neviem preco to ukazuje
-# + pristupy
 
-# hej, viem ze to vyzera napicu zatial, aj backgound je napicu jasne, viem.. ale treba tu funkcionalitu upravit dorobit..
-
-class InventarPlanner(Tk):
+class InventoryPlanner(Tk):
 
     def __init__(self):
         Tk.__init__(self)
 
-        self.title('Inventar Planner')
+        self.title('Inventory Planner')
         self.geometry('1280x720')
         self.resizable(0, 0)
         self._frame = None
         self.switch_frame(HomePage)
 
     def switch_frame(self, frame_class):
-        """Destroys current frame and replaces it with a new one."""
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
@@ -31,6 +23,21 @@ class InventarPlanner(Tk):
 
 
 class HomePage(Frame):
+
+    def sign_up(self):
+        root = Toplevel(width=400, height=220)
+        sign_up_label = Label(root, width=100, height=60)
+        sign_up_label.place(x=10, y=10)
+        Label(sign_up_label, text="Enter your username and password!", font="Times 12 bold").place(x=100, y=10)
+        Label(sign_up_label, text="User Name").place(x=50, y=50)
+        user_name = Entry(sign_up_label, bd=5)
+        user_name.place(x=150, y=50)
+        Label(sign_up_label, text="Password").place(x=50, y=100)
+        password = Entry(sign_up_label, bd=5)
+        password.place(x=150, y=100)
+        sign_up_button = Button(sign_up_label, text='Sign Up', command=root.destroy)
+        sign_up_button.place(x=180, y=150)
+        sign_up_label.mainloop()
 
     def __init__(self, controller):
         Frame.__init__(self, controller)
@@ -52,7 +59,7 @@ class HomePage(Frame):
         self.user = user_img
 
         home_label.create_image(640, 360, image=background_img)
-        home_label.create_text(640, 60, fill='blue', font="Times 40 bold", text='INVENTAR PLANNER')
+        home_label.create_text(640, 60, fill='blue', font="Times 40 bold", text='INVENTORY PLANNER')
 
         home_label.create_image(300, 360, image=products_img, tag='prod')
         home_label.create_text(300, 520, font="Times 28 bold", text='Products', tag='prod')
@@ -68,8 +75,8 @@ class HomePage(Frame):
 
         home_label.create_image(1180, 60, image=user_img, tag='signup')
         home_label.create_text(1180, 100, font="Times 12 bold", text='Sign up', tag='signup')
-        #               tuna este treba pridat nejake dialogove okno na prihlasenie sa
-        #home_label.tag_bind('signup', '<ButtonPress-1>', lambda event: controller.switch_frame(HelpPage))
+
+        home_label.tag_bind('signup', '<ButtonPress-1>', lambda event: self.sign_up())
 
 
 class ProductsPage(Frame):
@@ -98,31 +105,55 @@ class ProductsPage(Frame):
 
 class SettingsPage(Frame):
 
+    #def set_reminder(self):
+     #   if not self.is_reminder_set:
+     #       self.text_color.set("grey")
+     #       self.max_months.set(0)
+      #  else:
+       #     self.text_color.set("grey")
+        #    self.max_months.set(12)
+
     def __init__(self, controller):
         Frame.__init__(self, controller)
 
         self.controller = controller
+        self.is_reminder_set = False
 
-        settings_label = Canvas(self, width=1280, height=720)
-        settings_label.pack(side="top", fill="x")
+        self.background_img = PhotoImage(file="resources/background.gif")
+        self.back_img = PhotoImage(file="resources/back.gif")
 
-        background_img = PhotoImage(file="resources/background.gif")
-        self.bg = background_img
+        settings_canvas = Canvas(self, width=1280, height=720)
+        settings_canvas.pack(side='top', fill='x')
+        self.settings_label = Label(self, width=130, height=30)
+        self.settings_label.place(x=190, y=110)
 
-        settings_label.create_image(640, 360, image=background_img)
-        settings_label.create_text(640, 60, fill='blue', font="Times 40 bold", text='Settings')
+        settings_canvas.create_image(640, 360, image=self.background_img)
+        settings_canvas.create_text(640, 60, fill='blue', font="Times 40 bold", text='Settings')
 
-        back_img = PhotoImage(file="resources/back.gif")
-        self.back = back_img
+        settings_canvas.create_image(100, 60, image=self.back_img, tag='back')
+        settings_canvas.tag_bind('back', '<Button-1>', lambda event: controller.switch_frame(HomePage))
 
-        settings_label.create_image(100, 60, image=back_img, tag='back')
-        settings_label.tag_bind('back', '<Button-1>', lambda event: controller.switch_frame(HomePage))
+        Label(self.settings_label, font="Times 20 bold", text='Set up reminder').place(x=200, y=50)
+        #self.reminder = Checkbutton(self.settings_label, width=4, variable=self.is_reminder_set,
+        #                            onvalue=True, offvalue=False, command=self.set_reminder())
+        #self.reminder.place(x=400, y=59)
 
-        settings_label.create_text(300, 300, font="Times 20 bold", text='Set up reminder')
-        settings_label.create_text(300, 350, font="Times 20 bold", text='Remind upcoming ending of a product')
-        settings_label.create_text(300, 400, font="Times 20 bold", text='Limit at the given month: ')
+        self.text_color = "black"
+        self.max_months = 12
+        self.reminder_label = Label(self.settings_label, foreground=self.text_color, font="Times 20 bold",
+                                    text='Remind upcoming ending of a product          months before')
+        self.months = Spinbox(self.settings_label, from_=0, to=self.max_months, width=3, font='Helvetica 14 bold')
 
-        save_btn = Button(text ="Save", font="Times 20 bold", command=lambda: controller.switch_frame(HomePage))
+        self.reminder_label.place(x=120, y=110)
+
+        self.months.place(x=580, y=118)
+
+        Label(self.settings_label, font="Times 20 bold", text='Limit at the given month: ').place(x=200, y=170)
+        self.entry = Entry(self.settings_label, bd=5, font='Helvetica 14 bold')
+        self.entry.place(x=510, y=168, width=100)
+        Label(self.settings_label, font="Times 20 bold", text='€').place(x=620, y=170)
+
+        save_btn = Button(text="Save", font="Times 20 bold", command=lambda: controller.switch_frame(HomePage))
         save_btn.place(x=640, y=620)
 
 
@@ -150,5 +181,5 @@ class HelpPage(Frame):
 
 
 if __name__ == "__main__":
-    app = InventarPlanner()
+    app = InventoryPlanner()
     app.mainloop()
